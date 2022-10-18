a5804518962bf3e4b9d51473470506410e7c2c67
ivan <ivanagustiniag@gmail.com>
2022-10-18 19:55:27 -0300

    desafio mysql

---
+++ db/dbConnection.js
@@ -0,0 +1,10 @@
+const connection = {
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        database:'productos',
+    }
+}
+export default  connection;
